6f2f46e1a28953da8011fad35477130f3979b8f6
Krishna Tanwar <kstanwar0000@gmail.com>
2024-09-14 23:33:28 +0530

    JAVA basics

---
+++ ApnaCollege/output.java
@@ -0,0 +1,19 @@
+
+
+    public static void main(String args[]){
+        
+        System.out.println("Hello World with java");
+
+        System.out.print("Hello World with java\nwith krishna");
+
+        System.out.print("Hello World with java\n");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("*****");
+
+       
+
+
+    }
+
---
+++ ApnaCollege/variables.java
@@ -0,0 +1,23 @@
+public class variables {
+    public static void main(String[] args) {
+         //variables
+
+         String name = "krishna";
+         int age = 20;
+         double price = 2.25;
+         int a = 25;
+         int b = 50;
+ 
+         b = 20;
+         int sum = a + b;
+         name = "tanwar";
+         System.out.println(name);
+         System.out.println(sum);
+ 
+         int diff = a - b;
+         int mul = a * b;
+         System.out.println(diff);
+        
+    }
+    
+}
