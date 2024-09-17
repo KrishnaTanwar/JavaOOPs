38a8106145cd9610adc87e6281f3068192f6d682
Krishna Tanwar <kstanwar0000@gmail.com>
2024-09-17 16:02:19 +0530

    do_while_example & prime_number

--- ApnaCollege/marks_menu.java
+++ ApnaCollege/marks_menu.java
@@ -4,12 +4,27 @@ public class marks_menu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-    
-        do{
-            System.out.println(n);
-            
+        int flag = 0;
+
+        if(n==1){
+            do{
+                flag = 1;
+                int a = sc.nextInt();
+                if(a>=90){
+                    System.out.println("This is Good");
+                }
+                else if(a <= 89 && a >= 60){
+                    System.out.println("This is also Good");
+                }
+                else{
+                    System.out.println("This is Good as well");
+                }
+                if (a == 0){
+                    flag = 0;
+                }
+            }
+            while(flag!=0) ;
         }
-        while(n==1);
     }
     
 }
--- ApnaCollege/prime_number.java
+++ ApnaCollege/prime_number.java
@@ -4,14 +4,17 @@ public class prime_number {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int count = 0;
-        for(int i = 1;i<(n/2 + 1);i++){
-            if 
-            if(count==0){
-                System.out.println("Prime");
-            }
-            else{
-                System.out.println("Not PRime");
-            }
+        for(int i = 2;i<(n/2 + 1);i++){
+            if(n%i==0){
+                count+=1;
+            }            
+        }
+        System.out.println(count);
+        if(count==0){
+            System.out.println("Prime");
+        }
+        else{
+            System.out.println("Not PRime");
         }
     }
     
