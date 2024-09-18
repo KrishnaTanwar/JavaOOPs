023af05fe46ba5014aaed95a09763f3a120a640c
Krishna Tanwar <kstanwar0000@gmail.com>
2024-09-18 07:01:29 +0530

    Pattern Questions

---
+++ ApnaCollege/inverted_180_half_pyramid.java
@@ -0,0 +1,23 @@
+public class inverted_180_half_pyramid {
+    public static void main(String[] args) {
+        int n = 4;
+
+        //outer loop
+
+        for(int i = 1; i <= n; i++){
+            //inner loop -> Space
+            for(int j = 1; j<= n-i;j++){
+                System.out.print(" ");
+            }
+
+            //inner loop -> star
+
+            for(int j = 1; j<=i;j++){
+                System.out.print("*");
+            }
+            
+        
+        System.out.println();
+    }}
+    
+}
